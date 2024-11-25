263a100587c3a930d6ef769dba0948afa409c8b6
laphilbin <laphilbin@gmail.com>
2024-11-24 21:59:51 -0500

    format details

--- include.js
+++ include.js
@@ -9,8 +9,21 @@ async function getText(xtitle) {
   {
       if (obj.books[i].title===decodedURL) { break; }
   }
-  document.getElementById("booktitle").innerHTML = obj.books[i].title;
+  
   document.getElementById("cover240").src = "covers/" + obj.books[i].cover240;
+  document.getElementById("booktitle").innerHTML = obj.books[i].title;
+  document.getElementById("heatlevel").innerHTML = obj.books[i].heatlevel;
+  document.getElementById("length").innerHTML = obj.books[i].length;
+  document.getElementById("series").innerHTML = "series:" + obj.books[i].series;
+  document.getElementById("genre").innerHTML = obj.books[i].genre;
+  document.getElementById("keyword").innerHTML = obj.books[i].keyword;
+  document.getElementById("pub").innerHTML = "published by:" + obj.books[i].pub;
+  document.getElementById("date").innerHTML = obj.books[i].date;
+  document.getElementById("tagline").innerHTML = obj.books[i].tagline;
+  document.getElementById("Amz").href = "http://www.amazon.com/dp/" + obj.books[i].Amz;
+  document.getElementById("Nook").href = "http://www.barnesandnoble.com/s/" + obj.books[i].Nook;
+  document.getElementById("Kobo").href = "https://store.kobobooks.com/en-us/ebook/" + obj.books[i].Kobo;
+  document.getElementById("iTune").href = "https://itunes.apple.com/us/book/" + obj.books[i].iTune;
 }
 
 function getTitle(){
